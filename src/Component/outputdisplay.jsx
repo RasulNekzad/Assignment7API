d41aa12f917fe60663aa22deeb3e57772e9b1fac
Kaifeng <1536771545kai@gmail.com>
2023-06-16 18:32:04 -0400

    Complete

--- src/Component/outputdisplay.jsx
+++ src/Component/outputdisplay.jsx
@@ -9,31 +9,22 @@ const [input,setInput] = useState ("")
     async function fetchInfo(){
         const result = await axios.get(`https://zip-api.eu/api/v1/info/US-${input}`) 
         console.log(result)
-        //props.country_code+"-"+props.postal_code);
-        setOutputState(); //returns place_name from the json
+        setOutputState(result.data); 
+        
     }
-
-    
-
-const handleSubmit = (e)=>{
-    //prevents page from refreshing
-    //e.preventDefault();
-    console.log(e.target.value)
-};
-console.log(input)
-
 return(
     
     <div>
-         {/* <form> */}
+
        <input type="text" onChange={e=>{setInput(e.target.value)}}/> 
        <button onClick = {fetchInfo}> Submit </button> 
-    {/* </form> */}
         <h1>The place(s) in the US associated with that zip is </h1>
-        {/* {outputState.map((query)=>{
-        //query is what user entered in the form
-        return <p key = {query.postal_code}>{query.place_name}</p>
-    })} */}
+        <p>State: {outputState.state}</p>
+        <p>City: {outputState.place_name}</p>
+        <p>Latitude: {outputState.lat}</p>
+        <p>Longitude: {outputState.lng}</p>
+
+        
     </div>
     );
     
